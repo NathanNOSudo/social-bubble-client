a22a0e0dcc1b919ceb398a27cd4963915aecc925
NathanNOSudo <nathandavid719@gmail.com>
2020-12-02 12:48:49 -0600

    installed jsonwebtoken

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import "../App.css";
-import { response } from "express";
+// import { response } from "express";
 
 export default function Registration() {
   const [usernameReg, setUsernameReg] = useState("");
